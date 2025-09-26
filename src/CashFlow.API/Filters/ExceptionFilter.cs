c25377602e92bc274975ac0d569cb021f9ab306a
Raphael Costa Pettinato <raphael.petinatti@gmail.com>
2025-09-26 11:30:48 +0200

    Resource languages, added filters and validators.

---
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,52 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnkownError(context);
+        }
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            var ex = (ErrorOnValidationException)context.Exception;
+
+            var errorResponse = new ResponseErrorJson(ex.Errors);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+        else
+        {
+            var errorResponse = new ResponseErrorJson(context.Exception.Message);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+    }
+
+    private void ThrowUnkownError(ExceptionContext context)
+    {
+        var errorMessage = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorMessage);
+    }
+
+
+}
